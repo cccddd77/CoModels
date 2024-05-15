3203d4a155b5e5329bfd82772684d65852a281bb
chende <chende16@foxmail.com>
2024-05-15 10:46:02 +0000

    change setting

--- cv/classification/resnet50/train.sh
+++ cv/classification/resnet50/train.sh
@@ -2,12 +2,14 @@ export PYTHONPATH=$PWD:$PYTHONPATH
 set -aux
 
 GPU_NUMS=8
+NODE_NUMS=2
 PORT=12346
 MODEL_ARCH="resnet50"
 
 python3 -m oneflow.distributed.launch \
         --nproc_per_node $GPU_NUMS \
-        --master_addr 127.0.0.1 \
+        --nnodes $NODE_NUMS \
+        --master_addr 192.168.1.27 \
         --master_port $PORT \
         main.py \
         --cfg configs/default_settings.yaml \
