b2db4407fb61d041c504edf125eb07842a9487d3
chende <chende16@foxmail.com>
2024-05-15 06:07:40 +0000

    add num of nodes for resnet50.

--- cv/classification/resnest50/train.sh
+++ cv/classification/resnest50/train.sh
@@ -2,13 +2,15 @@ export PYTHONPATH=$PWD:$PYTHONPATH
 set -aux
 
 GPU_NUMS=4
+NODE_NUMS=4
 PORT=12377
 MODEL_ARCH="resnest50"
 IMAGE_SIZE=224
 
 python3 -m oneflow.distributed.launch \
         --nproc_per_node $GPU_NUMS \
-        --master_addr 127.0.0.1 \
+        --nnodes $NODE_NUMS \
+        --master_addr 192.168.1.27 \
         --master_port $PORT \
         main.py \
         --cfg configs/resnest_default_settings.yaml \
